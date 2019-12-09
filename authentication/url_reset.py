9c74a87169436f8688ebc9249d4d50a24a962d30
Miklos Sarosi <miklos.sarosi@gmail.com>
2019-12-09 22:27:31 +0000

    password reset templates modified

--- authentication/url_reset.py
+++ authentication/url_reset.py
@@ -3,7 +3,13 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
-    url('^$', password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
+    url('^$', 
+        password_reset,
+        {
+            'post_reset_redirect': reverse_lazy('password_reset_done'),
+            # 'html_email_template_name': 'registration/password_reset_html_email.html', FOR LATER IF I WANT HTML EMAIL
+        },
+        name='password_reset'),
     url(r'^done/$', password_reset_done, name='password_reset_done'),
     url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
         {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
