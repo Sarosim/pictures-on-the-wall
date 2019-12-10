acdeebb3e9ba796e6db938f95b8b609461ec6b71
Miklos Sarosi <miklos.sarosi@gmail.com>
2019-12-10 13:10:20 +0000

    email authentication added

--- pictures_on_the_wall/settings.py
+++ pictures_on_the_wall/settings.py
@@ -114,6 +114,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'authentication.backends.EmailAuth'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
