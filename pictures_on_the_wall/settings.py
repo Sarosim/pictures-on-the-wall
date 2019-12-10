f0d591fc79c5410b282f65bdc3905a7aa047f039
Miklos Sarosi <miklos.sarosi@gmail.com>
2019-12-10 14:08:22 +0000

    bootstrap added for styling

--- pictures_on_the_wall/settings.py
+++ pictures_on_the_wall/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication'
+    'authentication',
+    'django_forms_bootstrap'
 ]
 
 MIDDLEWARE = [
