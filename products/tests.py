9db3fef2464802ef69ca93cc879e9ed9225ecef0
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-03-05 20:19:43 +0000

    test corrected for the product model

--- products/tests.py
+++ products/tests.py
@@ -4,12 +4,8 @@ from .models import Product, Category
 # Create your tests here.
 
 class TestProduct(TestCase):
+
     def test_title(self):
         title_testing = Product(title = "Test Title")
         self.assertEqual(str(title_testing), "Test Title")
         self.assertNotEqual(str(title_testing), "something else")
-
-    """ def test_repro_fee(self):
-        repro_fee_testing = Product(base_repro_fee = 9.99)
-        self.assertEqual(repro_fee_testing = 9.99)
-        self.assertNotEqual(repro_fee_testing = 10) """
