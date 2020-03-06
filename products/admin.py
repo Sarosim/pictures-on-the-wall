2d3b6b2638a6ec07e960b15788f86d88589cf1ef
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-03-06 13:14:31 +0000

    models extended in products app to incluse necessary fields

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Artist, Technologies, Rating, Badge
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Artist)
+admin.site.register(Technologies)
+admin.site.register(Rating)
+admin.site.register(Badge)
