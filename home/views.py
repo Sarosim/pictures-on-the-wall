8c558fbf0e8df9ff986c4b7622e4406817f45a76
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-03-19 13:48:32 +0000

    copy of the landing page without intro created

--- home/views.py
+++ home/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def index(request):
     """A view that displays the landing page"""
     return render(request, "index.html")
+
+def index_no_intro(request):
+    """The view that displays the landing page without the intro so users already on the page /
+    don't get disrupted bu the intro every time they go back to the home page"""
+    return render(request, 'index_no_intro.html')
