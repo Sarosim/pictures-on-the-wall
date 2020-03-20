59b864cdce0ab6ead1eb3b956db28283cd8346a4
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-03-20 11:18:14 +0000

    detailed product view added with url and the html page too

--- products/views.py
+++ products/views.py
@@ -7,3 +7,8 @@ def all_products(request):
     """ The view rendering a page with all products listed """
     all_the_products = Product.objects.all()
     return render(request, "products.html", {"products": all_the_products})
+
+def product_details(request):
+    """ The view rendering the page for one selected product and all of its details """
+    selected_product = Product.objests.get(pk = id)
+    return render(request, "product_details.html", {"selected_product": selected_product})
