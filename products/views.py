780752ce63282845401b7c62c0f83553e8153964
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-03-26 20:56:32 +0000

    hashtags filtered and passed for the selected product's detailed view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product, Category
+from .models import Product, Category, Hashtag
 
 # Create your views here.
 
@@ -11,4 +11,17 @@ def all_products(request):
 def product_details(request, id):
     """ The view rendering the page for one selected product and all of its details """
     selected_product = Product.objects.get(pk = id)
-    return render(request, "product_details.html", {"selected_product": selected_product})
+    print("XXXXXXXXXXXXXXXXXXX sel_prod: {}".format(selected_product))
+    hashtags = Hashtag.objects.filter(product = selected_product).distinct()
+    for hashtag in hashtags:
+        print("XXXXXXXXXXXXXXXXXXX Hash-tags: {}".format(hashtag.hashtag))
+    pass_to_template = [
+        selected_product,
+        hashtags
+    ]
+    print("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY pass_to_templ.: {}!".format(pass_to_template[1]))
+    return render(
+        request, 
+        "product_details.html",
+        {"pass_to_template": pass_to_template}
+        )
