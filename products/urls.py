aa0b486732a65a77a113dbb4717635c90d0b6a64
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-03-30 20:51:27 +0000

    filtering added to products app, urlpattern and view created

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from products.views import all_products, product_details
+from products.views import all_products, product_details, filtered_products
 
 urlpatterns = [
     url(r'^$', all_products, name = 'products'),
+    url(r'^filtered/(?P<filter_group>\w+)/(?P<filter_name>\w+)', filtered_products, name = "filtered_products"),
     url(r'^product_details/(?P<id>\d+)', product_details, name = "product_details"),
 ]
--- products/views.py
+++ products/views.py
@@ -13,6 +13,7 @@ def product_details(request, id):
     selected_product = Product.objects.get(pk = id)
     # filtering the hashtags associated with the selected product - excluding duplicates:
     hashtags = Hashtag.objects.filter(product = selected_product).distinct()
+    print(f"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxwwwwwwwwwwwwwwwww**********selected_product : {hashtags}")
     # filtering all the relevant ratings from the Rating model:
     ratings = Rating.objects.filter(product = selected_product)
     ratings_total = 0
@@ -36,3 +37,25 @@ def product_details(request, id):
         "ratings_data": ratings_data
     }
     return render(request, "product_details.html", {"pass_to_template": pass_to_template})
+
+
+def filtered_products(request, filter_group, filter_name):
+    """ The view rendering a page for all products (Artwork) filtered by a user selected criteria """
+    
+    # setting the values for the variables for quick testing before actual links properly set on page
+    filter_group = 'room'
+    filter_name = 'bathroom'
+    
+    #constructing kwargs for the filter
+    if filter_group == 'hashtag':
+        filter_subgroup = 'hashtag'
+    else:
+        filter_subgroup = filter_group + '_name'
+    print(f"************************************************The function was called and the arguments are: -filter_name: {filter_name};  -filter_group: {filter_group}")
+    filter_kwargs = filter_group + '__' + filter_subgroup + '__' + 'iexact'
+
+    # the actual filtering
+    filtered_products = Product.objects.filter(**{ filter_kwargs: filter_name })
+
+    print(filtered_products)
+    return render(request, "products.html", {"products": filtered_products})
