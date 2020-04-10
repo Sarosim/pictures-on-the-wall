afb7fa128e8f30d0620148cfed188d54a572c1f3
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-04-10 15:42:49 +0000

    artist profile form moved to artist app from products

--- artist/urls.py
+++ artist/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import dashboard
+from .views import dashboard, artist_profile
 
 urlpatterns = [
     url(r'^$', dashboard, name = 'dashboard'),
+    url(r'^artist_profile/', artist_profile, name = "artist_profile"),
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import Product, Category, Hashtag, Rating, Artist, Size
 from .forms import FileUploadForm  # probably superseded, to be deleted
-from .forms import EditProductFormOne, EditProductFormThree, ArtistProfileForm
+from .forms import EditProductFormOne, EditProductFormThree
 from django.contrib.auth.decorators import login_required
 from pictures_on_the_wall.utils import special_filter
 
@@ -124,32 +124,3 @@ def edit_artwork(request):
         }
     )
 
-@login_required
-def artist_profile(request):
-    """ The view rendering the page for Artist profile creation, modification
-    or deletion """
-    if request.method == 'POST':
-        # User submitted an Artist profile form
-        artist_form = ArtistProfileForm(request.POST)
-        if artist_form.is_valid():
-            # Save the form
-            artist_form.save()
-            # Prepare the form for uploading a new artwork and render the page
-            set_artist = Artist.objects.filter(
-                assigned_user=request.user).values('id')[0]['id']
-            edit_form_one = EditProductFormOne(initial={'artist': set_artist})
-            edit_form_three = EditProductFormThree()
-            return render(request, 'edit.html',
-                {
-                    'edit_form_one': edit_form_one,
-                    'edit_form_three': edit_form_three,
-                }
-                )
-        else:
-            print("Form isn't valid, to be handled later")
-    else:
-        # An empty Profile form instance (with id)
-        artist_form = ArtistProfileForm(
-            initial={'assigned_user': request.user.id})
-
-    return render(request, 'artist_profile.html', {'artist_form': artist_form})
