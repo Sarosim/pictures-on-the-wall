548be4f1917b14959dc5b24c01ee633528d90c0b
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-04-10 16:03:45 +0000

    artist dashboard view checks if user is artist

--- artist/views.py
+++ artist/views.py
@@ -12,6 +12,20 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard(request):
     """ The view rendering the Arist dashboard """
+    # Getting the logged in user from the request
+    # and finding the corresponding artist in the Artist model
+    set_artist = Artist.objects.filter(assigned_user=request.user)
+    print(set_artist)
+    if set_artist:
+        # The logged in user has an artist associated 
+        return render(request, 'dashboard.html')
+    else:
+        # The logged in user doesn't have an artist profile")
+        # Create an empty Profile form instance (with id)
+        artist_form = ArtistProfileForm(
+            initial={'assigned_user': request.user.id})
+        return render(request, "artist_profile.html", {'artist_form': artist_form})
+
     return render(request, 'dashboard.html')
 
 
