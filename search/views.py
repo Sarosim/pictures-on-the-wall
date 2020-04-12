6de568af492f0fbafc93de8108ae7f40581c9ea6
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-04-12 14:41:51 +0000

    navbar search extended to 'category' and 'hashtag' models

--- search/views.py
+++ search/views.py
@@ -4,6 +4,10 @@ from products.models import Product, Category
 # Create your views here.
 def search_by_title(request):
     """ Search amongst the Artworks (products) by their title """
-    products = Product.objects.filter(title__icontains=request.GET['search'])
-
+    products_by_title = Product.objects.filter(title__icontains=request.GET['search'])
+    print(request.GET['search'])
+    products_by_category = Product.objects.filter(category__category_name__icontains=request.GET['search'])
+    products = products_by_title | products_by_category
+    products_by_hashtag = Product.objects.filter(hashtag__hashtag__icontains=request.GET['search'])
+    products = products | products_by_hashtag
     return render(request, 'products.html', {"products": products})
