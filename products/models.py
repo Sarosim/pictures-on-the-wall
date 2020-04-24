f10cc8ecedf275f8896a405ee454646ea1990061
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-04-24 19:57:24 +0000

    hashtag model changed, Product ForeignKey removed

--- products/models.py
+++ products/models.py
@@ -122,7 +122,6 @@ class Size(models.Model):
 
 class Hashtag(models.Model):
     """ Model for Products ratings: """
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, default='')
     hashtag = models.CharField(max_length=32, default='#ilovephotography')
 
     def __str__(self):
