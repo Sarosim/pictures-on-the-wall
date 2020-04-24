4a140da215c30c72f2a039a1863b77b178b453ff
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-04-24 20:01:53 +0000

    hashtag field added to Product model

--- products/models.py
+++ products/models.py
@@ -95,6 +95,7 @@ class Product(models.Model):
     num_of_orders = models.IntegerField(default=0)
     num_of_views = models.IntegerField(default=0)
     date_uploaded = models.DateTimeField(auto_now=True)
+    hashtag = models.CharField(max_length=32, default='', null=True)
 
     def __str__(self):
         return self.title
