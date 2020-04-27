b397ce10bedb1f747184f2d1fc4a21a531c1e1cf
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-04-27 21:19:44 +0000

    session created for the selected size for the cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from products.models import Product, Size, Technology
 
 def cart_contents(request):
     """ Ensuring the cart contents are available for every page to be rendered """
     
     cart = request.session.get('cart', {})
     """ A cart requesting a session with the existing cart if there is one, or a blank dictionary, if there's not. """
+    size_session = request.session.get('size_session', {})
     
+
     cart_items = []
     total = 0
     product_count = 0
@@ -15,5 +17,17 @@ def cart_contents(request):
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.base_repro_fee
         product_count += quantity
+        # 
+        # print("SIZES", size)
+        # current_technology = product.available_technologies
+        # print("CURRENT TECH", current_technology)
         cart_items.append({'id': id, 'quantity': quantity, 'product': product})
-    return {'cart_items': cart_items, 'total': total, 'product_count': product_count}
+
+    cart_item_sizes = []
+
+    for id, size in size_session.items():
+        product = get_object_or_404(Product, pk=id)
+        print("SIZE: ", size)
+        cart_item_sizes.append({'id': id, 'size': size})
+
+    return {'cart_items': cart_items, 'total': total, 'product_count': product_count, 'cart_item_sizes': cart_item_sizes}
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from products.models import Product
+from products.models import Product, Size
 import math
 
 # Create your views here.
@@ -14,15 +14,21 @@ def add_to_cart(request, id):
     (no quantity is obtained, as 99.99% of the customers will buy only one copy of the artwork)"""
 
     cart = request.session.get('cart', {})
+    size_session = request.session.get('size_session', {})
+
+    product = get_object_or_404(Product, pk=id)
     
     if id in cart:
         # If the product is in the cart already, it increases the quantity 
         cart[id] = int(cart[id]) + 1      
     else:
-        # If it's not in the cart it add it.
+        # If it's not in the cart it adds it.
         cart[id] = cart.get(id, 1) 
+        # we pick one size for the product for the cart
+        size_session[id] = size_session.get(id, Size.objects.filter(format_name=product.aspect_ratio).first().size_name)
 
     request.session['cart'] = cart
+    request.session['size_session'] = size_session
     return redirect(reverse('products'))
 
 
@@ -82,10 +88,13 @@ def update_cart(request, id):
     print(price)
 
     cart = request.session.get('cart', {})
+    size_session = request.session.get('size_session', {})
 
     if qty > 0:
         cart[id] = qty
         print('cart[id] igy nez ki: ', cart[id])
+        size_session[id] = selected_size
+        print('cart_session[id] igy nez ki: ', size_session[id])
     else:
         cart.pop(id)
 
