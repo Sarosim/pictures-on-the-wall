f0d9aa37b08de376fddc348b223b8e8e9c301046
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-04-30 12:20:29 +0000

    SBoto3 user warning fix: set AWS_DEFAULT_ACL = None,

--- pictures_on_the_wall/settings.py
+++ pictures_on_the_wall/settings.py
@@ -165,6 +165,9 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+# The default behavior of S3Boto3Storage is insecure...set AWS_DEFAULT_ACL = None,
+AWS_DEFAULT_ACL = None
+
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
